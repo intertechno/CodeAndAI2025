0979eefa57b413878971c33668e891ab1040cf3f
Jani Järvinen <jani.jarvinen@intertechno.org>
2026-02-10 14:23:35 +0200

    Added a very simple unit test into the ASP.NET web application.

---
+++ DotNet/WebAppWithUnitTests/WebAppWithUnitTests.Tests/Test1.cs
@@ -0,0 +1,21 @@
+﻿using WebAppWithUnitTests.Logic;
+
+namespace WebAppWithUnitTests.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculations calculations = new();
+            int a = 100;
+            int b = 200;
+
+            int expected = a + b;
+            int result = calculations.CalculateSum(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ DotNet/WebAppWithUnitTests/WebAppWithUnitTests/Logic/Calculations.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppWithUnitTests.Logic
+{
+    public class Calculations
+    {
+        public int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
